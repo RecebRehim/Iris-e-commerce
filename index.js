1b8c94263ad16bcd9fbc2a0a290f4d8a24d14979
RecebRehim <recebrehim05@gmail.com>
2024-01-12 02:58:32 +0400

    Add express template
    Express template is added to host the website

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Statik dosyaları servis etmek için 'views' klasörünü kullan
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  // index.html dosyasını gönder
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/cart', (req, res) => {
+  // index.html dosyasını gönder
+  res.sendFile(path.join(__dirname, 'views', 'cart.html'));
+});
+
+app.get('/account', (req, res) => {
+  // index.html dosyasını gönder
+  res.sendFile(path.join(__dirname, 'views', 'account.html'));
+});
+
+app.get('/product', (req, res) => {
+  // index.html dosyasını gönder
+  res.sendFile(path.join(__dirname, 'views', 'product.html'));
+});
+app.get('/productDetail', (req, res) => {
+  // index.html dosyasını gönder
+  res.sendFile(path.join(__dirname, 'views', 'productDetail.html'));
+});
+
+
+
+app.listen(port, () => {
+  console.log("Server is running at http://localhost:${port}");
+});
+
